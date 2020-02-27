4ed186ea44f69ccbf83ca0f76163bc85dea560c7
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-27 15:50:03 -0700

    Move to seperate folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App.js';
+import App from './Components/App/App.js';
 import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
