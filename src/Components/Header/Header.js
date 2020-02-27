64115e08056c2eb00225f2ba946dec3d98803a9c
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-27 16:01:44 -0700

    Add Header component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <nav className='nav-bar'>
+      <Link to='/' className='logo-box'>
+        <div className='logo'></div>
+        <h1>TBT</h1>
+      </Link>
+      <section className='links-container'>
+        <NavLink className='nav-btn' to='/about' type='button'>About</NavLink>
+        <NavLink className='nav-btn' to='/saved' type='button'>Saved(0)</NavLink>
+        <NavLink className='nav-btn' to='/current' type='button'>Current</NavLink>
+        <NavLink className='nav-btn' to='/search' type='button'>Search</NavLink>
+      </section>
+    </nav>
+  )
+}
+
+export default Header
