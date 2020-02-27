6b8226210b429207d74bd429891517b25b808522
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-27 16:02:37 -0700

    Add reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { eventsReducer } from './eventsReducer';
+
 
 const rootReducer = combineReducers({
-
+  events: eventsReducer,
 });
 
 export default rootReducer;
