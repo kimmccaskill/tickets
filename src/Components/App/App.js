ba0d9697aa7b94b167e2064300afc5f62a9c4261
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-28 11:22:36 -0700

    Change import path

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header'
-import Home from '../Home/Home'
+import Home from '../../Container/Home/Home'
 
 function App() {
   return (
