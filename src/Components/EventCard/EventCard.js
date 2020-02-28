cced72298a8c32ec7eb718d4228ebd14b1908566
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-28 16:52:57 -0700

    Add checkdate

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './EventCard.css'
+import moment from 'moment';
+let todaysDate = (moment().format('YYYY-MM-DDTHH:mm:ss') + "Z")
 
-const EventCard = ({ name,images,sales }) => {
+const EventCard = ({ name,images,sales,dates }) => {
+  const checkDate = () => {
+    return sales.public.startDateTime > todaysDate ? <p>On Sale on {sales.public.startDateTime}</p> : null
+  }
   return (
     <div className='event-card'>
-      <p>On Sale on {sales.public.startDateTime}</p>
+      {checkDate()}
+      <p>Event Date: {dates.start.localDate}</p>
       <p>{name}</p>
       {/* <img src={`${images[0].url}`}></img> */}
     </div>
