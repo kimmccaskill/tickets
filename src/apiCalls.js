382979d7f6ce7fc936cf16ba02cb3a8523e89ad9
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-28 11:21:44 -0700

    Add fetch for current events

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 import { apiKey } from './env'
 let tomorrowsDate = moment().add(1,'days').format('YYYY-MM-DD')
 
-export const getTomorrowsEvents = async () => {
+export const getUpcomingEvents = async () => {
   return await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?${apiKey}&onsaleOnAfterStartDate=${tomorrowsDate}`)
     .then(res => {
       if(!res.ok) {
@@ -10,5 +10,14 @@ export const getTomorrowsEvents = async () => {
       }
       return res.json()})
     .then(events => events._embedded.events)
+}
 
+export const getCurrentEvents = async () => {
+  return await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?${apiKey}&startDateTime=${tomorrowsDate}`)
+    .then(res => {
+      if(!res.ok) {
+        throw Error('Something is not right, try again later')
+      }
+      return res.json()})
+    .then(events => console.log(events._embedded.events))
 }
