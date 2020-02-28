0fa32cc4c66b1753172aaee26fef581f77466bb6
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-28 16:52:38 -0700

    Add set current

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -2,3 +2,8 @@ export const setUpcoming = upcomingEvents => ({
   type: 'SET_UPCOMING',
   upcomingEvents
 })
+
+export const setCurrent = currentEvents => ({
+  type: 'SET_CURRENT',
+  currentEvents
+})
