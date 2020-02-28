5f2ade194f13a628425ccd193a4e4dd70e570e30
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-28 16:52:48 -0700

    Add currentEventsPage component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header'
 import Home from '../../Container/Home/Home'
+import CurrentEventsPage from '../../Container/CurrentEventsPage/CurrentEventsPage'
 
 function App() {
   return (
     <main className="App">
       <Header />
       <Route exact path='/' component={Home} />
+      <Route exact path='/current' component={CurrentEventsPage} />
     </main>
   );
 }
