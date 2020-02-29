3482c68a8271db9585af6ec4270d0d158b96805f
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-29 14:36:06 -0700

    Import and combine reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { upcomingEventsReducer } from './upcomingEventsReducer';
 import { currentEventsReducer } from './currentEventsReducer';
+import { searchedEventsReducer } from './searchedEventsReducer';
+
 
 const rootReducer = combineReducers({
   upcomingEvents: upcomingEventsReducer,
   currentEvents: currentEventsReducer,
+  searchedEvents: searchedEventsReducer,
 });
 
 export default rootReducer;
