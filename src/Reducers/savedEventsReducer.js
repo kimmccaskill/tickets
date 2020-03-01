02d468089c50a6a798c4a0e6605572ff99e11281
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-29 17:37:04 -0700

    Adjust object format

--- src/Reducers/savedEventsReducer.js
+++ src/Reducers/savedEventsReducer.js
@@ -2,7 +2,7 @@ export const savedEventsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SAVE_EVENT':
       console.log(action)
-      return [...state, {event: action.event} ]
+      return [...state, action.event ]
     default:
       return state;
   }
