2520081642ac4cb246bf688b47ca20eeff6be475
Kim McCaskill <kimmccaskill92@gmail.com>
2020-02-29 17:37:56 -0700

    Move EventCard to container folder

--- src/Container/EventCard/EventCard.js
+++ src/Container/EventCard/EventCard.js
@@ -5,19 +5,21 @@ import { saveEvent } from '../../Actions'
 import moment from 'moment';
 let todaysDate = (moment().format('YYYY-MM-DDTHH:mm:ss') + "Z")
 
-const EventCard = ({ name,sales,dates, saveEvent }) => {
+const EventCard = ({ name, sales, dates, saveEvent }) => {
   let eventDate = moment(dates.start.localDate).format('MMMM DD YYYY')
   let saleDate = moment(sales.public.startDateTime).format('MMMM DD YYYY')
   const checkDate = () => {
     return sales.public.startDateTime > todaysDate ? <p>On Sale on {saleDate}</p> : null
   }
 
-  const submitSave = () => {
-    saveEvent({
-      name,
-      eventDate,
-      saleDate,
-  })
+const submitSave = () => {
+  saveEvent({
+    name,
+    eventDate,
+    saleDate,
+    dates,
+    sales
+})
     //below is temporary
     alert(`${name} has been saved!`)
   }
