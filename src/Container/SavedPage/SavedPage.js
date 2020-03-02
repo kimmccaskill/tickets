2c5748e586b686ff0a1c212fa65e679a95a27219
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-02 09:15:35 -0700

    Add error handing for empty events

--- src/Container/SavedPage/SavedPage.js
+++ src/Container/SavedPage/SavedPage.js
@@ -4,10 +4,14 @@ import EventsContainer from '../../Components/EventsContainer/EventsContainer.js
 import { connect } from 'react-redux';
 
 export const Home = ({ savedEvents }) => {
+  const checkSaved = () => {
+    return savedEvents.length > 0 ? <EventsContainer events={savedEvents}/> : <h2 className='error'>There are no saved events.</h2>
+  }
+
   return (
     <section className='saved-page'>
       <h2 className='saved-page-title'>Your Saved Events</h2>
-      <EventsContainer events={savedEvents}/>
+      {checkSaved()}
     </section>
   )
 }
