61128143a179f64bd3251b0b1a4bd1106245abd1
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-02 09:15:18 -0700

    Add unsave event reducer

--- src/Reducers/savedEventsReducer.js
+++ src/Reducers/savedEventsReducer.js
@@ -2,6 +2,8 @@ export const savedEventsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SAVE_EVENT':
       return [...state, action.event]
+    case 'UNSAVE_EVENT':
+      return state.filter(event => event.id !== action.id)
     default:
       return state;
   }
