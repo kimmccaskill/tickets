fa45e6a46d06724bdc017cf7347ebf44cb942f68
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-01 20:30:24 -0700

    Remove console log

--- src/Reducers/savedEventsReducer.js
+++ src/Reducers/savedEventsReducer.js
@@ -1,8 +1,7 @@
 export const savedEventsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SAVE_EVENT':
-      console.log(action)
-      return [...state, action.event ]
+      return [...state, action.event]
     default:
       return state;
   }
