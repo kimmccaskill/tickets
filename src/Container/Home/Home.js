dc812482e8e7aa53a28b66402557ec8403d06758
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-02 14:53:02 -0700

    Fix redundant alt tag

--- src/Container/Home/Home.js
+++ src/Container/Home/Home.js
@@ -16,7 +16,7 @@ export const Home = ({ upcomingEvents, setUpcoming }) => {
           <p className='homepage-description'>Trying to find out about tickets to your favorite band before anyone else?</p> 
           <p className='homepage-description'><em>TicketCloud</em> is here for you.</p>
         </div>
-        <img className='homepage-img' alt='Image of concert' src='https://i1.wp.com/weallwantsomeone.org/wp-content/uploads/2019/09/09-28-19_Vulfpeck-MSG76.jpg?fit=600%2C450&ssl=1'></img>
+        <img className='homepage-img' alt='Concert' src='https://i1.wp.com/weallwantsomeone.org/wp-content/uploads/2019/09/09-28-19_Vulfpeck-MSG76.jpg?fit=600%2C450&ssl=1'></img>
       </div>
       <h2 className='homepage-title'>What's Coming Up?</h2>
       <EventsContainer events={upcomingEvents}/>
