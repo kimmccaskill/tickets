1708b76aad41941bdaa37a6406e1f19e8b8bc661
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-02 13:26:23 -0700

    Test app snapshot

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let wrapper
+  it('should match the snapshot', () => {
+    wrapper = shallow(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
