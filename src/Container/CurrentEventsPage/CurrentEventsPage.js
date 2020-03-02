56fde0781ac65e1a6c58a07474872a3f789f3615
Kim McCaskill <kimmccaskill92@gmail.com>
2020-03-02 14:53:19 -0700

    Fix bug of repeating image header load

--- src/Container/CurrentEventsPage/CurrentEventsPage.js
+++ src/Container/CurrentEventsPage/CurrentEventsPage.js
@@ -1,32 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './CurrentEventsPage.css'
 import EventsContainer from '../../Components/EventsContainer/EventsContainer'
 import { connect } from 'react-redux'
 import { setCurrent } from '../../Actions'
 import { getCurrentEvents} from '../../apiCalls/apiCalls'
 
+export class CurrentEventsPage extends Component {
+  componentDidMount() {
+    const { setCurrent } = this.props
 
-export const CurrentEventsPage = ({currentEvents, setCurrent}) => {
-  
-  const getEvents = () => {
     getCurrentEvents()
-    .then(currentEvents => setCurrent(currentEvents))
+      .then(currentEvents => setCurrent(currentEvents))
   }
-  getEvents()
-  const shuffled = currentEvents.sort(() => 0.5 - Math.random());
-  let randomEvents = shuffled.slice(0, 5);
 
-  const randomImgs = randomEvents.map((event, i) => <img alt={event.name} className='img-in-header' src={event.images[Math.round(Math.random() * 5)].url} />)
-
-  return (
-    <section>
-      <div className='img-header'>
-        {randomImgs}  
-      </div>
-      <h1 className='current-pg-title'>On Sale Now!</h1>
-      <EventsContainer events={currentEvents}/>
-    </section>
-  )
+  render() {
+    const { currentEvents } = this.props
+    const shuffled = currentEvents.sort(() => 0.5 - Math.random()).slice(0, 5);
+    const randomImgs = shuffled.map(event => <img alt={event.name} className='img-in-header' src={event.images[2].url} />)
+    return (
+      <section>
+        <div className='img-header'>
+          {randomImgs}  
+        </div>
+        <h1 className='current-pg-title'>On Sale Now!</h1>
+        <EventsContainer events={currentEvents}/>
+      </section>
+    )
+  }
 }
 
 export const mapStateToProps = state => ({
